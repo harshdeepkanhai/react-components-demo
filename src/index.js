dbe4e2a04a197e7870daf9b259eaed87c032ec85
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2021-12-30 19:57:51 +0530

    Component Reuse

--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
   return (
     <div className="ui container comments">
+        <ApprovalCard>
+            <div>
+                <h4>Warning!</h4>
+                Are you sure you want to do this?
+            </div>
+        </ApprovalCard>
       <ApprovalCard>
         <CommentDetail
             author="Sam"
@@ -15,19 +21,22 @@ const App = () => {
             avatar={faker.image.avatar()}
         />
       </ApprovalCard>
-      
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 2:00PM"
-        content="I like the subject"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Yesterday at 5:00PM"
-        content="I like the writing"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+            author="Alex"
+            timeAgo="Today at 2:00PM"
+            content="I like the subject"
+            avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+            author="Jane"
+            timeAgo="Yesterday at 5:00PM"
+            content="I like the writing"
+            avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
